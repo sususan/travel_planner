50e33e39779f22114f490db67cc16b6736978741
Su Su San <SuSan.Su@mediacorp.com.sg>
2025-10-31 22:15:30 +0800

    Trigger CI/CD

---
+++ planner_agent/lambda/final_formatter_agent.py
@@ -0,0 +1,15 @@
+import logging
+
+from planner_agent.orchestrator.orchestrator import plan_itinerary
+from planner_agent.tools.s3io import get_json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+    logger.info("!! lambda_handler !!")
+    name = event.get("name", "world")
+    return {
+        "statusCode": 200,
+        "body": f"Hello, {name}!"
+    }
